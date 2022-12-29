424fa27d439eda23742be4711fe7810d143c193c
Julia Kram <yuliya.kramarenko@gmail.com>
2022-12-29 19:58:57 +0000

    Increase text size.

--- test/js/test_text_latyn1.js
+++ test/js/test_text_latyn1.js
@@ -1,11 +1,11 @@
 function test_text_latyn1() {
     return `Nič bula temna, jak ljoh.
 
-    Vmyvajus' holodnoju vodoju na ĝanku, kolo vikna šelestyt' lystja javora.
+Vmyvajus' holodnoju vodoju na ĝanku, kolo vikna šelestyt' lystja javora.
     
-    Jogo prydileno do l'vivs'kogo garnizonu, pry najblyžčim avansi jogo maly imenuvaty majorom.
+Jogo prydileno do l'vivs'kogo garnizonu, pry najblyžčim avansi jogo maly imenuvaty majorom.
     
-    U zv'jazku iz podijamy na Jevromajdani svjaščenyky L'vivs'koji arhyjeparhiji UGKC ščogodyny b'jut' u cerkovni dzvony po 5 hvylyn.
+U zv'jazku iz podijamy na Jevromajdani svjaščenyky L'vivs'koji arhyjeparhiji UGKC ščogodyny b'jut' u cerkovni dzvony po 5 hvylyn.
     
-    Aby znyzyty riven' notyfikacijnogo šumu, v Teleĝrami možna vybraty opciju "NE SPOVIŠČATY" dlja okremyh kanaliv.`;
+Aby znyzyty riven' notyfikacijnogo šumu, v Teleĝrami možna vybraty opciju "NE SPOVIŠČATY" dlja okremyh kanaliv.`;
 }
