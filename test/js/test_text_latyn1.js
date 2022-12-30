91229dafb54337b900bdb8f0f6ef8082038b5deb
Julia Kram <yuliya.kramarenko@gmail.com>
2022-12-30 01:22:45 +0000

    Format text and style.

--- test/js/test_text_latyn1.js
+++ test/js/test_text_latyn1.js
@@ -1,16 +1,6 @@
 function test_text_latyn1() {
-    return `Nič bula temna, jak ljoh.
-
-Vmyvajus' holodnoju vodoju na ĝanku, kolo vikna šelestyt' lystja javora.
-    
-Jogo prydileno do l'vivs'kogo garnizonu, pry najblyžčim avansi jogo maly imenuvaty majorom.
-    
-U zv'jazku iz podijamy na Jevromajdani svjaščenyky L'vivs'koji arhyjeparhiji UGKC ščogodyny b'jut' u cerkovni dzvony po 5 hvylyn.
-    
-Aby znyzyty riven' notyfikacijnogo šumu, v Teleĝrami možna vybraty opciju "NE SPOVIŠČATY" dlja okremyh kanaliv.
-
-V slovah inšomovnogo pohodžennja m’jakyj znak pyšet'sja pislja m’jakyh d, t, z, s, l, n pered ji, jo, a takož pered ja, ju, je, jaki čytajut'sja jak [ja], [ju], [je]: ad'je, konferans'je, monpans'je, pas'jans, atel'je, barel'jef, batal'jon, mil'jard, buton'jerka, vin'jetka, kan'jon, Got'je, N'ju-Jork, N'juton, N'jasa, Ren'je toščo. Ale V’jent'jan.
-
-M’jakyj znak ne pyšet'sja pislja b, p, v, m, f, r, ž, č, š, šč: drib, prygolub, sypte, step, krov, visim, verf, niž, již, znaješ, bačyš, pič, rič, boršč, moršč, vorotar, girkyj, kobzar, perevir, teper, Harkiv toščo. Vynjatok: Gor'kyj, trjoh.
-
+    return `    Nič bula temna, jak ljoh. Vmyvajus' holodnoju vodoju na ĝanku, kolo vikna šelestyt' lystja javora. Jogo prydileno do l'vivs'kogo garnizonu, pry najblyžčim avansi jogo maly imenuvaty majorom.
+    U zv'jazku iz podijamy na Jevromajdani svjaščenyky L'vivs'koji arhyjeparhiji UGKC ščogodyny b'jut' u cerkovni dzvony po 5 hvylyn. Aby znyzyty riven' notyfikacijnogo šumu, v Teleĝrami možna vybraty opciju "NE SPOVIŠČATY" dlja okremyh kanaliv.
+    V slovah inšomovnogo pohodžennja m’jakyj znak pyšet'sja pislja m’jakyh d, t, z, s, l, n pered ji, jo, a takož pered ja, ju, je, jaki čytajut'sja jak [ja], [ju], [je]: ad'je, konferans'je, monpans'je, pas'jans, atel'je, barel'jef, batal'jon, mil'jard, buton'jerka, vin'jetka, kan'jon, Got'je, N'ju-Jork, N'juton, N'jasa, Ren'je toščo. Ale V’jent'jan.
+    M’jakyj znak ne pyšet'sja pislja b, p, v, m, f, r, ž, č, š, šč: drib, prygolub, sypte, step, krov, visim, verf, niž, již, znaješ, bačyš, pič, rič, boršč, moršč, vorotar, girkyj, kobzar, perevir, teper, Harkiv toščo. Vynjatok: Gor'kyj, trjoh.
 `;}
