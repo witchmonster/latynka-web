7f092ac717a95f03a2287d38b00451dac20bae1d
Julia Kram <yuliya.kramarenko@gmail.com>
2022-12-31 13:02:48 +0000

    Fix output textarea style.

--- test/js/testcases/test_texts.js
+++ test/js/testcases/test_texts.js
@@ -4,8 +4,8 @@ function testText(testName) {
     М'який знак не пишеться після б, п, в, м, ф, р, ж, ч, ш, щ: дріб, приголуб, сипте, степ, кров, вісім, верф, ніж, їж, знаєш, бачиш, піч, річ, борщ, морщ, воротар, гіркий, кобзар, перевір, тепер, Харків тощо. Виняток: Горький, трьох.
     Особливі випадки: "НЕ СПОВІЩАТИ", сьогодні, льох (але майор), священники, працюю, "Ще".`,
         'latin1': `    V slovah inšomovnogo pohodžennja m’jakyj znak pyšet'sja pislja m’jakyh d, t, z, s, l, n pered ji, jo, a takož pered ja, ju, je, jaki čytajut'sja jak [ja], [ju], [je]: ad'je, konferans'je, monpans'je, pas'jans, atel'je, barel'jef, batal'jon, mil'jard, buton'jerka, vin'jetka, kan'jon, Got'je, N'ju-Jork, N'juton, N'jasa, Ren'je toščo. Ale V’jent'jan.
-        M’jakyj znak ne pyšet'sja pislja b, p, v, m, f, r, ž, č, š, šč: drib, prygolub, sypte, step, krov, visim, verf, niž, již, znaješ, bačyš, pič, rič, boršč, moršč, vorotar, girkyj, kobzar, perevir, teper, Harkiv toščo. Vynjatok: Gor'kyj, trjoh.
-        Osoblyvi vypadky: "NE SPOVIŠČATY", sjogodni, ljoh (ale major), svjaščennyky, pracjuju, "Šče".`
+    M’jakyj znak ne pyšet'sja pislja b, p, v, m, f, r, ž, č, š, šč: drib, prygolub, sypte, step, krov, visim, verf, niž, již, znaješ, bačyš, pič, rič, boršč, moršč, vorotar, girkyj, kobzar, perevir, teper, Harkiv toščo. Vynjatok: Gor'kyj, trjoh.
+    Osoblyvi vypadky: "NE SPOVIŠČATY", sjogodni, ljoh (ale major), svjaščennyky, pracjuju, "Šče".`
     }
     return tests[testName] ? tests[testName] : '';
 }
