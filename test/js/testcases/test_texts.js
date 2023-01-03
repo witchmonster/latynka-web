fb909f54c6542c608f056f7d349d9e7f87ae88c3
Julia Kram <yuliya.kramarenko@gmail.com>
2023-01-03 13:10:42 +0000

    Remove jajko (polish).

--- test/js/testcases/test_texts.js
+++ test/js/testcases/test_texts.js
@@ -5,7 +5,7 @@ function testText(testName) {
     В латинці правильно писати апостроф перед "йо"/"jo" після приголосних: серйозно (ser’jozno), курйоз (kur’joz), але район, мільйон.
     Особливі випадки на диграфи: сьогодні, льох (але майор), священник, працюю.
     Особливі випадки зі змішаним регістром: "НЕ СПОВІЩАТИ" (але "Ще"), "ЮНЕСКО", СЕРЙОЗНО, КУРЙОЗ, РАЙОН, МІЛЬЙОН.
-    Особливі випадки на 'йо': його, "йогурт", (Йоркшир), /йога/, [Йоганнесбург], \`Йовович\`, яйко, але "яйок" (jajok).`,
+    Особливі випадки на 'йо': його, "йогурт", (Йоркшир), /йога/, [Йоганнесбург], \`Йовович\`.`,
         'latin1': `        V slovah inšomovnogo pohodžennja m’jakyj znak pyšet'sja pislja m’jakyh d, t, z, s, l, n pered ji, ’jo, a takož pered ja, ju, je, jaki čytajut'sja jak [ja], [ju], [je]: ad'je, konferans'je, monpans'je, pas'jans, atel'je, barel'jef, batal'jon, mil'jard, buton'jerka, vin'jetka, kan'jon, Got'je, N'ju-Jork, N'juton, N'jasa, Ren'je toščo. Ale V’jent'jan.
     M’jakyj znak ne pyšet'sja pislja b, p, v, m, f, r, ž, č, š, šč: drib, prygolub, sypte, step, krov, visim, verf, niž, již, znaješ, bačyš, pič, rič, boršč, moršč, vorotar, girkyj, kobzar, perevir, teper, Harkiv toščo. Vynjatok: Gor'kyj, trjoh.
     V latynci pravyl'no pysaty apostrof pered "jo"/"jo" pislja prygolosnyh: ser’jozno (серйозно), kur’joz (серйозно), ale rajon, mil'jon.
