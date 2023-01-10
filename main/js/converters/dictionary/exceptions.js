cbfe3b052eff764e4529d8f0f09007445f125e2b
Julia Kram <yuliya.kramarenko@gmail.com>
2023-01-10 14:49:08 +0000

    Fixed missing button in light theme

--- main/js/converters/dictionary/exceptions.js
+++ main/js/converters/dictionary/exceptions.js
@@ -3,6 +3,13 @@ const exceptions = {
         maxLength: 4,
         4: {
             '\"ьо\"': '\"\'o\"',
+        },
+        '"': {
+            '\"ьо\"': '\"\'o\"'
+        },
+        'р': {
+            'русск': 'moskal\'sk',
+            'російськ': 'moskal\'s\'k'
         }
     },
     latToCyr: {
